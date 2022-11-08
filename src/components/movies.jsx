a37f2a9d3cfb8d0913e984306b8b334d63a8f2dc
Daniele Trovato <plasticsoup@yahoo.co.uk>
2022-11-08 16:22:31 +0000

    Add 14-Adding All Genres

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -16,7 +16,9 @@ class Movies extends Component {
   };
 
   componentDidMount() {
-    this.setState({ movies: getMovies(), genres: getGenres() });
+    const genres = [{ name: "All Genres" }, ...getGenres()];
+
+    this.setState({ movies: getMovies(), genres });
   }
 
   handleDelete = (movie) => {
@@ -42,7 +44,7 @@ class Movies extends Component {
   };
 
   handleGenreSelect = (genre) => {
-    this.setState({ selectedGenre: genre });
+    this.setState({ selectedGenre: genre, currentPage: 1 });
   };
 
   filterMovies;
@@ -58,9 +60,12 @@ class Movies extends Component {
 
     if (count === 0) return <p>There are no movies in the database.</p>;
 
-    const filtered = selectedGenre
-      ? allMovies.filter((m) => m.genre._id === selectedGenre._id)
-      : allMovies;
+    // if selectedGenre is truthy and it has an _id attribute return the filtered
+    // movies, else return allMovies
+    const filtered =
+      selectedGenre && selectedGenre._id
+        ? allMovies.filter((m) => m.genre._id === selectedGenre._id)
+        : allMovies;
 
     const movies = paginate(filtered, currentPage, pageSize);
 
